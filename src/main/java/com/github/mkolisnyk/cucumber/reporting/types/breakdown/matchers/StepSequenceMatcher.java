49676ce9a4dab4ea8586372ee64b82a78df3eca8
mykolak <mykola.kolisnyk@thetrainline.com>
2015-12-11 14:42:59 +0000

    #47: Step sequence matcher was added

---
+++ src/main/java/com/github/mkolisnyk/cucumber/reporting/types/breakdown/matchers/StepSequenceMatcher.java
@@ -0,0 +1,29 @@
+package com.github.mkolisnyk.cucumber.reporting.types.breakdown.matchers;
+
+import com.github.mkolisnyk.cucumber.reporting.types.breakdown.DataDimension;
+import com.github.mkolisnyk.cucumber.reporting.types.result.CucumberScenarioResult;
+
+public class StepSequenceMatcher extends ComplexMatcher {
+
+    @Override
+    public boolean matches(CucumberScenarioResult result, DataDimension filter) {
+        int start = 0;
+        for (DataDimension filterItem : filter.getSubElements()) {
+            boolean found = false;
+            for (int i = start; i < result.getSteps().length; i++) {
+                String expression = filterItem.getExpression();
+                if (result.getSteps()[i].getName().equals(expression)
+                        || result.getSteps()[i].getName().matches(expression)) {
+                    start = i + 1;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}

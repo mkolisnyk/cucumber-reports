d9c907e02e26af41d6d3e91752fc287e63e1d83c
mykolak <mykola.kolisnyk@thetrainline.com>
2016-07-01 18:49:58 +0100

    #58: changes to build process. Preparation for refactoring.

---
+++ src/test-integration/java/com/github/mkolisnyk/cucumber/reporting/FileContentValidationIT.java
@@ -0,0 +1,11 @@
+package com.github.mkolisnyk.cucumber.reporting;
+
+import org.junit.Test;
+
+public class FileContentValidationIT {
+
+    @Test
+    public void testValidate() {
+        ;
+    }
+}

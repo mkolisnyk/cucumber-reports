eb124469861c49561ada8831eacbd24b61c16a3e
mykolak <mykola.kolisnyk@thetrainline.com>
2016-07-04 14:22:05 +0100

    #58: Corrections to fail build due to failed integration tests

--- src/test-integration/java/com/github/mkolisnyk/cucumber/reporting/FileContentValidationIT.java
+++ src/test-integration/java/com/github/mkolisnyk/cucumber/reporting/FileContentValidationIT.java
@@ -1,11 +1,12 @@
 package com.github.mkolisnyk.cucumber.reporting;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FileContentValidationIT {
 
     @Test
     public void testValidate() {
-        ;
+        Assert.assertTrue(true);
     }
 }

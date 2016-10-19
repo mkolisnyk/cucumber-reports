d412ef083dd59077a4d5d225f9c098c77aa2b6ea
mykolak <mykola.kolisnyk@thetrainline.com>
2016-10-19 10:06:51 +0100

    #96: ExtendedCucumberOptions annotation is set as inherited

--- cucumber-report-generator/src/main/java/com/github/mkolisnyk/cucumber/runner/ExtendedCucumberOptionList.java
+++ cucumber-report-generator/src/main/java/com/github/mkolisnyk/cucumber/runner/ExtendedCucumberOptionList.java
@@ -1,10 +1,12 @@
 package com.github.mkolisnyk.cucumber.runner;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Inherited
 @Target(ElementType.TYPE)
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface ExtendedCucumberOptionList {
--- cucumber-report-generator/src/main/java/com/github/mkolisnyk/cucumber/runner/ExtendedCucumberOptions.java
+++ cucumber-report-generator/src/main/java/com/github/mkolisnyk/cucumber/runner/ExtendedCucumberOptions.java
@@ -1,11 +1,13 @@
 package com.github.mkolisnyk.cucumber.runner;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Repeatable(ExtendedCucumberOptionList.class)
 @Target({ ElementType.TYPE })

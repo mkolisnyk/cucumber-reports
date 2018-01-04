92f58c176cfc008ecd103ad9d279684a1047056d
mkolisnyk <kolesnik.nickolay@gmail.com>
2018-01-04 13:41:09 +0000

    #150: format transformation changes for parallel runners

---
+++ cucumber-runner/src/test/java/com/github/mkolisnyk/cucumber/runner/CustomFormatter.java
@@ -0,0 +1,74 @@
+package com.github.mkolisnyk.cucumber.runner;
+
+import java.util.List;
+
+import gherkin.formatter.Formatter;
+import gherkin.formatter.NiceAppendable;
+import gherkin.formatter.model.Background;
+import gherkin.formatter.model.Examples;
+import gherkin.formatter.model.Feature;
+import gherkin.formatter.model.Scenario;
+import gherkin.formatter.model.ScenarioOutline;
+import gherkin.formatter.model.Step;
+
+public class CustomFormatter implements Formatter {
+    private NiceAppendable output;
+
+    public CustomFormatter(Appendable appendable) {
+        output = new NiceAppendable(appendable);
+        output.println("CustomFormatter()");
+        System.out.println("CustomFormatter(): " + output.toString());
+    }
+    @Override
+    public void background(Background arg0) {
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public void done() {
+    }
+
+    @Override
+    public void endOfScenarioLifeCycle(Scenario arg0) {
+    }
+
+    @Override
+    public void eof() {
+    }
+
+    @Override
+    public void examples(Examples arg0) {
+    }
+
+    @Override
+    public void feature(Feature arg0) {
+    }
+
+    @Override
+    public void scenario(Scenario arg0) {
+    }
+
+    @Override
+    public void scenarioOutline(ScenarioOutline arg0) {
+    }
+
+    @Override
+    public void startOfScenarioLifeCycle(Scenario arg0) {
+    }
+
+    @Override
+    public void step(Step arg0) {
+    }
+
+    @Override
+    public void syntaxError(String arg0, String arg1, List<String> arg2, String arg3, Integer arg4) {
+    }
+
+    @Override
+    public void uri(String arg0) {
+    }
+
+}
